0250f0deb0766fc9a1b5e6a01ca878acfa344f34
Tamsen1995 <tamanhbui95@gmail.com>
2024-02-13 19:48:23 -0500

    new custom file upload

--- src/components/FileUploadModal.tsx
+++ src/components/FileUploadModal.tsx
@@ -12,12 +12,15 @@ interface FileUploadModalProps {
 
 function useFileInput() {
   const [file, setFile] = useState<File | null>(null);
+  const [fileName, setFileName] = useState("No file selected");
 
   const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFile(event.target.files ? event.target.files[0] : null);
+    const selectedFile = event.target.files ? event.target.files[0] : null;
+    setFile(selectedFile);
+    setFileName(selectedFile ? selectedFile.name : "No file selected");
   };
 
-  return { file, onFileChange };
+  return { file, fileName, onFileChange };
 }
 
 const FileUploadModal: React.FC<FileUploadModalProps> = ({
@@ -26,8 +29,16 @@ const FileUploadModal: React.FC<FileUploadModalProps> = ({
   onSubmit,
   isLoading,
 }) => {
-  const { file: file1, onFileChange: onFile1Change } = useFileInput();
-  const { file: file2, onFileChange: onFile2Change } = useFileInput();
+  const {
+    file: file1,
+    fileName: fileName1,
+    onFileChange: onFile1Change,
+  } = useFileInput();
+  const {
+    file: file2,
+    fileName: fileName2,
+    onFileChange: onFile2Change,
+  } = useFileInput();
 
   return (
     <Modal
@@ -44,36 +55,45 @@ const FileUploadModal: React.FC<FileUploadModalProps> = ({
         >
           CLOSE{" "}
         </button>
+
         <div className="mb-6">
           <label
-            className="block text-gray-700 text-lg font-bold mb-2"
+            className="shadow h-14 appearance-none border-0 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 text-lg bg-gray-100 flex justify-between items-center cursor-pointer"
             htmlFor="file1"
           >
-            <CloudUploadIcon /> Choose first file
+            <span>
+              <CloudUploadIcon />
+            </span>
+            <span>{fileName1}</span>
+            <input
+              className="hidden"
+              type="file"
+              id="file1"
+              accept=".pdf"
+              onChange={onFile1Change}
+            />
           </label>
-          <input
-            className="shadow h-14 appearance-none border-0 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 text-lg bg-gray-100"
-            type="file"
-            id="file1"
-            accept=".pdf"
-            onChange={onFile1Change}
-          />
         </div>
+
         <div className="mb-6">
           <label
-            className="block text-gray-700 text-lg font-bold mb-2"
+            className="shadow h-14 appearance-none border-0 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 text-lg bg-gray-100 flex justify-between items-center cursor-pointer"
             htmlFor="file2"
           >
-            <CloudUploadIcon /> Choose second file
+            <span>
+              <CloudUploadIcon />
+            </span>
+            <span>{fileName2}</span>
+            <input
+              className="hidden"
+              type="file"
+              id="file2"
+              accept=".pdf"
+              onChange={onFile2Change}
+            />
           </label>
-          <input
-            className="shadow h-14 appearance-none border-0 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 text-lg bg-gray-100"
-            type="file"
-            id="file2"
-            accept=".pdf"
-            onChange={onFile2Change}
-          />
         </div>
+
         <button
           onClick={onSubmit}
           className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline"
